18b251b6d72d71adce9c21211ccc0dd6e17c703b
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 10:09:45 +0530

    update cors origin

--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://vintage-pi.vercel.app/', 
     credentials: true,             
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
