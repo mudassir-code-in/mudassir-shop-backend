ef3369a5afd2452860daced84edc570370e8452c
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 12:27:36 +0530

    fix transport verify

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -3,7 +3,8 @@ import nodemailer from 'nodemailer';
 
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp-relay.brevo.com', 
+    port: 587,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
