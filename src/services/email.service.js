b334f9a47454d4da78639d2d96da9940dc16ff08
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 18:52:59 +0530

    fix mail service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,14 +1,16 @@
 import nodemailer from 'nodemailer';
 
-
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',       
+    port: 465,               
+    secure: true,               
+    family: 4,                  
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
     },
-    connectionTimeout: 10000, 
-    greetingTimeout: 10000,   
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
     socketTimeout: 30000
 });
 
@@ -29,7 +31,6 @@ export const sendMail = async (to, subject, text, html) => {
             text,
             html
         });
-
         console.log('Email sent successfully:', info.messageId);
         return info;
     } catch (error) {
