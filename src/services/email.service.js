774946309167e6ebcf64e6445a119c88e9a1dd73
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 12:13:08 +0530

    fix transport verify

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -3,7 +3,9 @@ import nodemailer from 'nodemailer';
 
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -12,7 +14,7 @@ export const transporter = nodemailer.createTransport({
 
 
 export const sendMail = async (to, subject, text, html) => {
-    try{
+    try {
         const info = await transporter.sendMail({
             from: `Mudassir developer, ${process.env.EMAIL_USER}`,
             to,
