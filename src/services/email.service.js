2fba2615531bb3d33fa3b7b39cc6e06973c0a103
Mudassir <mudassir.code.in@gmail.com>
2026-06-14 15:15:28 +0530

    fix brevo service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,8 +1,5 @@
 import nodemailer from 'nodemailer';
 
-console.log("--- DEBUGGING EMAIL SETUP ---");
-console.log("EMAIL_USER check:", process.env.EMAIL_USER ? "Loaded!" : "NOT LOADED!");
-console.log("EMAIL_PASS check:", process.env.EMAIL_PASS ? "Loaded!" : "NOT LOADED!");
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
