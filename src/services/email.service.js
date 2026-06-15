6b6ec64bc3e60e71e449c392d5078fb1240db542
Mudassir <mudassir.code.in@gmail.com>
2026-06-15 08:39:47 +0530

    fix email service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,22 +1,37 @@
-import { Resend } from 'resend';
+import nodemailer from 'nodemailer';
 
-// API Key ko environment variable se lena
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+   service: 'gmail',                 
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 30000
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log('email connecting error', error);
+    } else {
+        console.log('Email server ready to send message');
+    }
+});
 
 export const sendMail = async (to, subject, text, html) => {
     try {
-        const response = await resend.emails.send({
-            from: 'onboarding@resend.dev', 
-            to: [to],
-            subject: subject,
-            text: text,
-            html: html
+        const info = await transporter.sendMail({
+            from: `"Mudassir Developer" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            text,
+            html
         });
-
-        console.log('Email sent successfully:', response?.id);
-        return response;
+        console.log('Email sent successfully:', info.messageId);
+        return info;
     } catch (error) {
-        console.error('Resend API Error:', error);
-        throw error; 
+        console.log('Email sending error', error);
+        throw error;
     }
 }
